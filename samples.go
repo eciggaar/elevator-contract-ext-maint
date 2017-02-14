a8624db81067969518c664449607005dbd78c692
eciggaar <edward@local>
2017-02-14 18:09:53 +0100

    Initial commit

---
+++ samples.go
@@ -0,0 +1,19 @@
+package main
+
+var samples = `
+{
+	"event": {
+		"maintenanceStop": true,
+		"maintenanceReason": "manual",
+		"assetID": "The ID of a managed asset. The resource focal point for a smart contract.",
+	},
+	"initEvent": {
+		"nickname": "ELEVATOR",
+		"version": "The ID of a managed asset. The resource focal point for a smart contract."
+	},
+	"state": {
+	"maintenanceStop": true,
+	"maintenanceReason": "manual",
+	"assetID": "The ID of a managed asset. The resource focal point for a smart contract.",
+	}
+}`
